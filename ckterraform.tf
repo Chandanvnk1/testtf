140300b6e4db47d825fc2e75e6b3e7045d91a6ff
Chandanvnk1 <53539214+Chandanvnk1@users.noreply.github.com>
2024-01-11 12:29:28 +0530

    Update ckterraform.tf

--- ckterraform.tf
+++ ckterraform.tf
@@ -8,12 +8,6 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-south-1a", "ap-south-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
-
   tags = {
     Terraform = "true"
     Environment = "dev"
