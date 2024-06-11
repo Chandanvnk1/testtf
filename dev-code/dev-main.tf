2de8c1dbfb3aab255e33fdf59ac2dbaa985f6796
Chandanvnk1 <53539214+Chandanvnk1@users.noreply.github.com>
2024-06-11 22:07:28 +0530

    Update dev-main.tf

--- dev-code/dev-main.tf
+++ dev-code/dev-main.tf
@@ -1,1 +1,15 @@
+provider "aws" {
+  region  = "ap-south-1"
+}
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "ck-gk"
+  cidr = "10.0.0.0/16"
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
