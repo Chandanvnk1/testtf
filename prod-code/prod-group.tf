e586223008efe0e8099c56c39f2c01aa63e76ad2
Chandanvnk1 <53539214+Chandanvnk1@users.noreply.github.com>
2024-06-11 22:52:28 +0530

    Create prod-group.tf

---
+++ prod-code/prod-group.tf
@@ -0,0 +1,10 @@
+module "iam_group_with_policies" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
+
+  name = "prodadmins"
+  
+  tags = {
+	Terraform = "true"
+	Environment = "prodgroup"
+   }
+}
