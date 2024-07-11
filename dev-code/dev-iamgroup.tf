ae0c4153a233507141f85fc80080474e7fc654ad
Chandanvnk1 <Chandanvnk1@live.com>
2024-07-11 22:07:17 +0530

    changed the code

--- dev-code/dev-iamgroup.tf
+++ dev-code/dev-iamgroup.tf
@@ -1,10 +1,4 @@
 module "iam_group_with_policies" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
-
-  name = "devadmins"
-  
-  tags = {
-	Terraform = "true"
-	Environment = "dev-group"
-   }
+  name = "dev-admins"
 }
--- prod-code/prod-iamgroup.tf
+++ prod-code/prod-iamgroup.tf
@@ -1,10 +1,4 @@
 module "iam_group_with_policies" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
-
-  name = "prodadmins"
-  
-  tags = {
-	Terraform = "true"
-	Environment = "prod-group"
-   }
+  name = "prod-admins"
 }
